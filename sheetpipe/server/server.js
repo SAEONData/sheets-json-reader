35911f3f8cb9986b80e755ca4d62960c18ee4d52
dcugh <diana@gmail.com>
2019-05-29 13:05:50 +0200

    commit express server for deployment

---
+++ sheetpipe/server/server.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+var { rows, doc }= require('./../sheetsapi/spreadsheet')
+
+app.get('/', function (req, res) {
+    res.send('hello saeon')
+})
+
+app.get('/spreadsheet', function(req, res) {
+    res.send(doc)
+})
+
+var server = app.listen(8081, function() {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('computer says yes at http://%s:%s', host, port)
+})
--- sheetpipe/sheetsapi/spreadsheet.js
+++ sheetpipe/sheetsapi/spreadsheet.js
@@ -3,7 +3,7 @@ let creds = require('./client_secret.json')
 
 let doc = new GoogleSpreadsheet('1mkcJSmxJJk6mM-Cv5dOiIikvaRqi6vezyCTm5a6atWI')
 
-doc.useServiceAccountAuth(creds, function (err){
+doc.useServiceAccountAuth(creds, function (err) {
   doc.getRows(1, function (err, rows) {
     console.log(rows)
   })
