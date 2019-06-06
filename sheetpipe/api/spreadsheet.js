ad6bb8daf5cf2481f99705a91adc7e90dafa7e26
dcugh <diana@gmail.com>
2019-06-06 10:30:21 +0200

    ammend location of written json to public folder

--- sheetpipe/api/spreadsheet.js
+++ sheetpipe/api/spreadsheet.js
@@ -2,13 +2,14 @@ let GoogleSpreadsheet = require('google-spreadsheet')
 let creds = require('./client_secret.json')
 let fs = require('fs')
 
+
 let doc = new GoogleSpreadsheet('1mkcJSmxJJk6mM-Cv5dOiIikvaRqi6vezyCTm5a6atWI')
 
 doc.useServiceAccountAuth(creds, function (err) {
   doc.getRows(1, function (err, rows) {
     console.log(rows)
-    fs.writeFile ("input.json", JSON.stringify(rows), function(err){
+    fs.writeFile ("../server/input.json", JSON.stringify(rows), function(err){
       console.log(err, 'rowsworks')
     })
   })
-})
+}) 
